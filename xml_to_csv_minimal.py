9f841bebf198297fc862bfc73951e9fac772ec2d
Jonas Massa <jmassa@mavericks.local>
2016-09-20 11:29:05 +0200

    acceleration

--- xml_to_csv_minimal.py
+++ xml_to_csv_minimal.py
@@ -2,10 +2,22 @@ import xml.etree.ElementTree as ET
 import optparse
 import csv
 
+attributeKeys = ['FRAME', 'name',\
+                 'POSITION_X', 'POSITION_Y', 'RADIUS',\
+                 'Coord_Minimum_0', 'Coord_Minimum_1',\
+                 'Coord_Maximum_0', 'Coord_Maximum_1',\
+                 'RegionRadii_0', 'RegionRadii_1',\
+                 'Count']
+                 
+attributes = ['trackID'] + attributeKeys
+
+nColRange = [87, 88, 89]
+
+minAttributes = 5
 
 def getTrackDict(root):
-    ''' get the Track ID for every object '''
-    track_ids_ = {}
+    """ get the Track ID for every object """
+    track_ids_ = {} 
     spotIDs = {}
     for frame in root[0][1]:
         for spot in frame:
@@ -26,7 +38,7 @@ def getTrackDict(root):
 
 if __name__ == '__main__':
 
-    parser = optparse.OptionParser(description='XML to CSV Converter')
+    parser = optparse.OptionParser(description='XML to CSV converter')
 
     parser.add_option('--input-xml', type=str, dest='input_xml',
                       help='Filename for the xml file')
@@ -41,15 +53,22 @@ if __name__ == '__main__':
 
     with open(opt.output_dir + '/mamut_table.csv', 'w') as featureTable:
         tablewriter = csv.writer(featureTable, delimiter=',')
-        tablewriter.writerow(['trackID'] + root[0][1][0][0].keys())
- 
+        tablewriter.writerow(attributes)
 
         track_ids = getTrackDict(root)
 
         for frame in root[0][1]:
             for spot in frame:
-                row = [track_ids[int(spot.attrib.get('ID'))]] + spot.attrib.values()
-                tablewriter.writerow(row)
+                row = [track_ids[int(spot.attrib.get('ID'))]]
+                nValues = len(spot.attrib.values())
 
+                if (nValues in nColRange):
+                    for key in attributeKeys:
+                        row += [spot.attrib.get(key)]
+                else:
+                    for i in range(minAttributes):
+                        row += [spot.attrib.get(attributeKeys[i])]
+
+                tablewriter.writerow(row)
 
     featureTable.close()
