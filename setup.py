84c4e229cd5773fadd0ceec067d267a7e82d1119
Carsten Haubold <carstenhaubold@googlemail.com>
2016-12-20 14:08:57 +0100

    Add setup.py so that the exporter can be installed as python module

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="MaMuTTrackingExport",
+    version="0.1.0",
+    author="Jonas Massa",
+    author_email="massa.jonas@iwr.uni-heidelberg.com",
+    description=("Export tracking results (from ilastik) in the format required by MaMuT"),
+    license="BSD",
+    keywords="cell tracking ilastik fiji",
+    url="https://github.com/Beinabih/MaMutConverter",
+    packages=['mamutexport'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7"
+    ],
+)
